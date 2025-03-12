861b993af8e531f4c914ec49861127e382b833fd
Paulo HFA <rickphtb@gmail.com>
2025-03-12 13:18:10 -0300

    deletarTransacao endpoint

--- src/main/java/com/example/demo/itau/controller/TransacaoController.java
+++ src/main/java/com/example/demo/itau/controller/TransacaoController.java
@@ -2,8 +2,8 @@ package com.example.demo.itau.controller;
 
 import com.example.demo.itau.model.Transacao;
 import com.example.demo.itau.service.TransacaoService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.service.annotation.DeleteExchange;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,15 +18,20 @@ public class TransacaoController {
     public TransacaoController(TransacaoService trasacaoService) {
         this.trasacaoService = trasacaoService;
     }
-
+    //endpoint para adicionar transacao em transacaos
     @PostMapping("/adicionartransacao")
     public void adicionarTransacoes(@RequestBody Transacao transacao){
         trasacaoService.adicionartransacao(transacao);
     }
-
+    //endpoint para exibir as transacoes adicionadas em transacaos
     @GetMapping("/exibirtransacao")
     public List<Transacao> exibirTransacoes(){
         System.out.println("Transações: " + trasacaoService.Listartransacoes());
         return trasacaoService.Listartransacoes();
+    }
+    //Endpoint para deletar a transação
+    @DeleteMapping("/deletar")
+    public void  deletarTransacao(@RequestBody Transacao transacao){
+        trasacaoService.removertransacao(transacao);
     }
 }
